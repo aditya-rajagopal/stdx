0b4a22383b52e565dd09451d68f9cd9ba4398abf
Aditya Rajagopal <adi.raj465@gmail.com>
2026-01-06 21:40:52 -0800

    fix: logFatal broke with zig upgrade

--- src/stdx.zig
+++ src/stdx.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const assert = std.debug.assert;
 
 pub const wav = @import("wav.zig");
 pub const ogg_vorbis = @import("ogg_vorbis.zig");
@@ -31,9 +32,10 @@ pub const Options = struct {
 pub fn logFatal(comptime format: []const u8, args: anytype) noreturn {
     var threaded = std.Io.Threaded.init_single_threaded;
     const io = threaded.ioBasic();
-    io.unlockStderr();
-    var stderr = std.Io.File.stderr().writer(io, &.{});
-    stderr.interface.print("ERROR: " ++ format ++ "\n", args) catch {};
+    var locked_stderr = io.lockStderr(&.{}, null) catch unreachable;
+    var stderr = locked_stderr.terminal();
+    defer io.unlockStderr();
+    stderr.writer.print("ERROR: " ++ format ++ "\n", args) catch {};
     std.process.exit(1);
 }
 
@@ -47,6 +49,17 @@ pub fn MB(mb: f32) usize {
 
 pub fn GB(gb: f32) usize {
     return @intFromFloat(gb * 1024 * 1024 * 1024);
+}
+
+pub fn divIntToFloat(comptime Float: type, numerator: anytype, denominator: anytype) Float {
+    const NType = @TypeOf(numerator);
+    const DType = @TypeOf(denominator);
+    comptime assert(@typeInfo(NType) == .int or @typeInfo(NType) == .float);
+    comptime assert(@typeInfo(DType) == .int or @typeInfo(DType) == .float);
+    comptime assert(@typeInfo(Float) == .float);
+    const n: Float = if (comptime @typeInfo(NType) == .int) @floatFromInt(numerator) else @floatCast(numerator);
+    const d: Float = if (comptime @typeInfo(DType) == .int) @floatFromInt(denominator) else @floatCast(denominator);
+    return n / d;
 }
 
 test {
