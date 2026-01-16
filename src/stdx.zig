88ee087ce85f4aa16e3e9e15eafa02263e055987
Aditya Rajagopal <adi.raj465@gmail.com>
2026-01-16 11:30:55 -0800

    feat: add cut functions for strings

--- src/stdx.zig
+++ src/stdx.zig
@@ -75,6 +75,16 @@ pub const inlineAssert = switch (builtin.mode) {
     }.assert,
 };
 
+pub fn cut(comptime T: type, haystack: []const T, needle: []const T) ?struct { []const T, []const T } {
+    const index = std.mem.find(T, haystack, needle) orelse return null;
+    return .{ haystack[0..index], haystack[index + needle.len ..] };
+}
+
+pub fn cutScalar(comptime T: type, haystack: []const T, needle: T) ?struct { []const T, []const T } {
+    const index = std.mem.findScalar(T, haystack, needle) orelse return null;
+    return .{ haystack[0..index], haystack[index + 1 ..] };
+}
+
 test {
     std.testing.refAllDecls(@This());
 }
